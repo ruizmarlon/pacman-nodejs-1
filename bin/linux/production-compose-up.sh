fdc9fc06ce92e6e7357aaba89bee067a5e335a02
Aaron Rueth <arueth@gmail.com>
2020-07-16 15:54:49 -0700

    Fixed local development scripts and added Mirantis logo (#6)
    * Fixed local development scripts and added Mirantis logo
    
    * Updating README file (#8)

--- bin/linux/production-compose-up.sh
+++ bin/linux/production-compose-up.sh
@@ -2,4 +2,4 @@
 
 BIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker-compose --file ${BIN_DIR}/../../swarm/docker-compose.production.yml --project-name pacman-prod up $@
+docker-compose --file ${BIN_DIR}/../../swarm/docker-compose.production.yml --project-name pacman-prod up --build $@
